810ff8f957b4317a59379fc6bf78fc2a5f09c763
Mickael MONGE <mickael@viti.pf>
2024-09-13 11:25:31 -1000

    UPD : Init map on French Polynesia

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Initialisation de la carte Leaflet
-var map = L.map('map').setView([20, 0], 2); // Vue initiale sur le monde
+//var map = L.map('map').setView([20, 0], 2); // Vue initiale sur le monde
+// Initialisation de la carte Leaflet avec focus sur la Polynésie française
+var map = L.map('map').setView([-17.6509, -149.4260], 8);  // Coordonnées de la Polynésie avec un zoom ajusté
 
 // Chargement des tuiles de la carte (OpenStreetMap)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
