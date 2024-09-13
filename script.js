65e4a47d6827f5cf22063935ef2a105abf479c8a
Mickael MONGE <mickael@viti.pf>
2024-09-12 16:45:34 -1000

    UPD : script.js

--- script.js
+++ script.js
@@ -7,21 +7,12 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-// Fonction pour ajouter des marqueurs à partir du fichier JSON
-function addDiveSites() {
-    fetch('sites.json')
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(site => {
-                // Ajout des marqueurs sur la carte
-                var marker = L.marker([site.lat, site.lng]).addTo(map);
-                
-                // Ajout d'un popup avec les informations du site
-                marker.bindPopup(`<b>${site.name}</b><br>Profondeur min: ${site.min_depth}m<br>Profondeur max: ${site.max_depth}m`);
-            });
-        })
-        .catch(error => console.error('Erreur lors du chargement des sites:', error));
-}
-
-// Appel de la fonction pour charger les sites de plongée
-addDiveSites();
+// Chargement des sites de plongée
+fetch('sites.json')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(site => {
+            L.marker([site.lat, site.lng]).addTo(map)
+                .bindPopup(`<b>${site.name}</b><br>Profondeur min: ${site.min_depth}m<br>Profondeur max: ${site.max_depth}m`);
+        });
+    });
